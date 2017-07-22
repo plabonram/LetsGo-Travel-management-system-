37fd3445a82d8a6ebb4f96871b6eaf2924465f80
plabon21 <plaboncsejnu21@gmail.com>
2017-07-22 12:05:40 +0600

    Initial Commit

---
+++ LetsGo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LetsGo.Startup))]
+namespace LetsGo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
